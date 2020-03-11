49ab801990e91ca1fc9e2229e71c61d05203139c
Leo Taylor <leotaylor723@gmail.com>
2020-03-11 14:36:40 -0500

    added client specific styling

--- src/views/resultsView/resultsView.js
+++ src/views/resultsView/resultsView.js
@@ -7,23 +7,23 @@ class ResultsView extends React.Component {
   }
 
   toggleView = (e) => {
-    console.log('click');
     this.setState({showResults: !this.state.showResults});
   };
 
   render () {
-    const welcomeText = this.state.showResults ? "Thank You For Voting" : "Please Rank The Below Performers";
+    const welcomeText = this.state.showResults ? "Thank You For Voting" : "Rank Your Favorite Performers";
 
     return (
       <div className="resultsContainer col-sm-12">
         <h1 className="resultsView">{welcomeText}</h1>
         <div className="resultDisplay">
           {this.state.showResults ?
-            <iframe className="col-xs-12 actualResults" src="https://embed.polleverywhere.com/ranking_polls/wGriQAnkOVaUoG78C6sbO?controls=none&short_poll=true" width="800" height="600" frameBorder="0" title="Rankings Results"></iframe>
+            // <iframe className="col-xs-12 actualResults" src="https://embed.polleverywhere.com/ranking_polls/wGriQAnkOVaUoG78C6sbO?controls=none&short_poll=true" width="800" height="600" frameBorder="0" title="Rankings Results"></iframe>
+            <iframe className="col-xs-12 actualResults"src="https://embed.polleverywhere.com/ranking_polls/aVYfM308HAfQEq2txN8af?controls=none&short_poll=true" width="800" height="600" frameBorder="0" title="Rankings Results"></iframe>
             :
             <div className='resultsContainer'>
               <iframe className="col-xs-12" src="https://pollev.com/leotaylor655" width="800" height="900" frameBorder="0" title="Rankings Poll"></iframe>
-              <button className="btn btn-danger col-xs-12 resultsButton" onClick={this.toggleView}>See Results</button>
+              <button className="btn col-xs-12 resultsButton" onClick={this.toggleView}>SEE RESULTS</button>
             </div>
           }
         </div>
